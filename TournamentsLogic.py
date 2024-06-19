98d73017f8b1f99ee655d01cf52463d489b7b8fa
ArPaVa <arianpzv@gmail.com>
2024-06-19 13:07:11 -0400

    Tournament Commit

--- TournamentsLogic.py
+++ TournamentsLogic.py
@@ -1,22 +1,48 @@
 from abc import ABC, abstractmethod
 import random
-class Board:
+import copy
 
+class Board:
     def __init__ (self):
         self.board = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]]
 
     def __getitem__(self,key):
         return self.board[key]
 
+class Player(ABC):
+    @abstractmethod
+    def Move(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class RandomPlayer(Player):
+    def __init__(self,name):
+        self.name = name
+    
+    def __str__(self):
+        return f"{self.name}"
+    def __repr__(self):
+        return f"{self.name}"
+    
+    def Move(self,board,move):
+        while True:
+            i = random.randint(0,2)
+            j = random.randint(0,2)
+            if board[i][j] == -1:
+                return i,j,move
+            
 class TicTacToe:
     def __init__ (self,player1, player2):
         self.board = Board()
         self.players = [player1,player2]
         self.turn = 0
         self.play = []
+        self.winner = -1
     
     def Ended(self):
-
         def i_winner(self,i):
             if (self.board[0][0]== i and self.board[1][1]== i and self.board[2][2]== i) or (self.board[0][2]== i and self.board[1][1]== i and self.board[2][0]== i):
                 return True
@@ -50,25 +76,65 @@ class TicTacToe:
                 self.turn = 0
             
             ended = TicTacToe.Ended(self)
+        
+        if self.winner == -1: #THIS IS NOT GOOD NEED WORK
+            random_winner = random.randint(0,1)
+            return (self.players[0],self.players[1],self.players[random_winner])
+
+        return (self.players[0],self.players[1],self.winner)
             
-class Player(ABC):
+class Tournament(ABC):
     @abstractmethod
-    def Move(self):
+    def __init__(self):
         pass
 
-class RandomPlayer(Player):
-    def __init__(self,name):
-        self.name = name
-    
-    def Move(self,board,move):
-        while True:
-            i = random.randint(0,2)
-            j = random.randint(0,2)
-            if board[i][j] == -1:
-                return i,j,move
+    @abstractmethod
+    def Run(self):
+        pass
 
-pepe = RandomPlayer("Pepe")
-ramon = RandomPlayer("Ramon")
-fight = TicTacToe(pepe,ramon)
-fight.Run()
-print(fight.play)
+class BridgeTournament(Tournament):
+    def __init__(self,players):
+        self.players_list = players
+        random.shuffle(self.players_list)
+        self.actual_bracket = copy.deepcopy(self.players_list)
+        self.next_bracket = []
+        self.pending = []
+
+    def Initial_check(self):
+        if len(self.players_list)%2 == 0:
+            return True
+        return False
+    
+    def Next_match(self):
+        first = random.randint(0,len(self.actual_bracket)-1)
+        second = random.randint(0,len(self.actual_bracket)-1)
+        while not first !=second:
+            second = random.randint(0,len(self.actual_bracket)-1)
+        self.pending.append((self.actual_bracket[first],self.actual_bracket[second]))
+        r_second = max(first,second)
+        r_first = min (first,second)
+        return self.actual_bracket.pop(r_second) , self.actual_bracket.pop(r_first)
+
+    def Ended(self):
+        if len(self.actual_bracket)== 0:
+            if len(self.next_bracket)==1:
+                return True
+            self.actual_bracket = self.next_bracket
+            self.next_bracket = []
+            return False
+        return False
+
+    def Run(self):
+        ended = False
+        if not self.Initial_check():
+            raise Exception("Invalid")
+        while not ended:
+            next = self.Next_match()
+            TTT = TicTacToe(next[0],next[1]).Run()
+            for i in range(len(self.pending)):
+                if self.pending[i] == (TTT[0],TTT[1]) or self.pending[i] == (TTT[1],TTT[0]):
+                    self.pending.pop(i)
+                    self.next_bracket.append(TTT[2])
+                    break
+            ended = self.Ended()
+        return self.next_bracket[0]
