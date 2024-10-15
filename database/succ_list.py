ee853a47fd938cfb252f0fe20e07f734449de990
aspio28 <aspiolea28@gmail.com>
2024-10-14 23:49:38 -0400

    fix succ_list

--- database/succ_list.py
+++ database/succ_list.py
@@ -9,17 +9,6 @@ class SuccList:
         self.boss_node = node
         self.one_node = True
 
-    def set_succ(self, node: ChordNodeReference):
-        actual_node = node.succ
-        for i in range(0, self.r):
-            if self.list[0].id == actual_node.id:
-                break
-            self.list.append(actual_node)
-            try:
-                actual_node = actual_node.succ
-            except Exception as e:
-                print(f"Error in create list of successors: {e}")
-
     def fix_succ(self):
         while True:
 
@@ -27,17 +16,18 @@ class SuccList:
                 self.list = [self.boss_node.succ]
                 self.one_node = False
 
-            if not self.one_node and self.boss_node.succ != self.list[0].id:
-                self.list = [self.boss_node.succ]
+            if not self.one_node:
+                if self.boss_node.succ != self.list[0].id:
+                    self.list = [self.boss_node.succ]
 
-            if not self.one_node and len(self.list) < self.r and self.list[-1].succ.id != self.boss_node.id:
-                self.list.append(self.list[-1].succ)
+                elif len(self.list) < self.r and self.list[-1].succ.id != self.boss_node.id:
+                    self.list.append(self.list[-1].succ)
 
-            else:
-                for i in range(0, len(self.list) - 1):
-                    if self.list[i].succ.id != self.list[i + 1].id:
-                        self.list.pop()
-                        self.list = self.list[:i] + self.list[i].succ + self.list[i:]
+                else:
+                    for i in range(0, len(self.list) - 1):
+                        if self.list[i].succ.id != self.list[i + 1].id:
+                            self.list.pop()
+                            self.list = self.list[:i] + self.list[i].succ + self.list[i:]
             time.sleep(10)
 
     def __str__(self) -> str:
